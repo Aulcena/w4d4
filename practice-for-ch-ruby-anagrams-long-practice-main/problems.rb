7c13696b5ba7fbd1ad5beec0cf9ac695232b6ec1
AJU <aulcena@gmail.com>
2023-07-20 16:48:22 -0400

    working on phase_4

--- practice-for-ch-ruby-anagrams-long-practice-main/problems.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/problems.rb
@@ -1,18 +1,36 @@
 def first_anagram?(str_1, str_2)
-    hs = Hash.new(0)
-    
-    str_1.each_char do |char|
-        hs[char] += 1
-    end
 
-    str_2.each_char do |char|
-        hs[char] -= 1
-        return false if hs[char] < 0
-    end
+    str_1_arr = str_1.split("")
+    str_2_arr = str_2.split("")
 
-    true
-    
+    str_1_arr.permutation.to_a.include?(str_2_arr)
+
+
 end
+
+p first_anagram?("gizmo", "sally")    #=> false
+p first_anagram?("elvis", "lives")    #=> true
+
+
+
+
+
+
+# def first_anagram?(str_1, str_2)
+#     hs = Hash.new(0)
+    
+#     str_1.each_char do |char|
+#         hs[char] += 1
+#     end
+
+#     str_2.each_char do |char|
+#         hs[char] -= 1
+#         return false if hs[char] < 0
+#     end
+
+#     true
+    
+# end
 
 
 
@@ -28,11 +46,16 @@ def second_anagram?(str_1, str_2)
     str_2.empty?
 end
 
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
 
 # Use bubble-sort algorithm
 def third_anagram?(str_1,str_2)
+
+    str_1 = bubble_sort(str_1)
+    str_2 = bubble_sort(str_2)
+
+    str_1 == str_2
     
 end
 
@@ -54,3 +77,6 @@ def bubble_sort(str)
     end
     str
 end
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
