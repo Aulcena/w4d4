112b78300fd2b23b71ee2eef1ad429b8bdfa9e9b
Fanyi <ft2582@columbia.edu>
2023-07-20 14:55:36 -0400

    finish largest sum phase 2

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/problems.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/problems.rb
@@ -26,40 +26,60 @@ end
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)  # =>  -5
 
+#phase 1
+# def largest_contiguous_sub_sum(array)
+
+#     new_arr = [] #1
+
+#     i = 0 #1
+#     #n^2
+#     while i < array.length # n
+#         j = i # 1
+#         while j < array.length # n
+
+#            new_arr << array[i..j] #1
+
+#             j += 1 #1
+
+#         end
+
+#         i += 1 #1
+
+#     end
+
+#     max_sum = 0 # 1
+#     #n
+#     new_arr.each do |sub_array| # n
+
+#         if sub_array.sum > max_sum # 1
+
+#             max_sum = sub_array.sum #1
+
+#         end
+
+#     end
+
+#     return max_sum #1
+
+# end
+
 def largest_contiguous_sub_sum(array)
-
-    new_arr = [] #1
-
-    i = 0 #1
-    #n^2
-    while i < array.length # n
-        j = i # 1
-        while j < array.length # n
-
-           new_arr << array[i..j] #1
-
-            j += 1 #1
-
+    largest_sum = 0
+    current_sum = 0
+    i =0
+    while i < array.length
+        if current_sum < 0
+            current_sum =0
         end
 
-        i += 1 #1
-
-    end
-
-    max_sum = 0 # 1
-    #n
-    new_arr.each do |sub_array| # n
-
-        if sub_array.sum > max_sum # 1
-
-            max_sum = sub_array.sum #1
-
+        current_sum+=array[i]
+        if current_sum > largest_sum
+            largest_sum = current_sum
         end
 
+        i+=1
     end
-
-    return max_sum #1
-
+    largest_sum
 end
 
 list = [5, 3, -7]
@@ -72,3 +92,4 @@ p largest_contiguous_sub_sum(list) # => 8
 # [3]           # => 3
 # [3, -7]       # => -4
 # [-7]          # => -7
+
